99acd4205a917876eccad348adefb77b8726caae
Politofr09 <deshidratatorpa@gmail.com>
2024-09-01 18:56:15 +0200

    Added custom support for premake

---
+++ premake5.lua
@@ -0,0 +1,44 @@
+project "yaml-cpp"
+    group "Dependencies"
+    kind "StaticLib"
+    language "C++"
+
+    targetdir ("bin/%{prj.name}")
+    objdir ("bin-obj/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
+
+	defines
+	{
+		"YAML_CPP_STATIC_DEFINE"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "off"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
