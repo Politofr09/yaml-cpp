7beb83189a1ae6f9b2d8c439056120f6ca487e51
Politofr09 <deshidratatorpa@gmail.com>
2024-09-28 18:31:38 +0200

    Updated gitignore and premake build script

--- premake5.lua
+++ premake5.lua
@@ -2,15 +2,17 @@ project "yaml-cpp"
     group "Dependencies"
     kind "StaticLib"
     language "C++"
+	systemversion "latest"
+	cppdialect "C++17"
+	staticruntime "on"
 
-    targetdir ("bin/%{prj.name}")
-    objdir ("bin-obj/%{prj.name}")
+    targetdir ("bin/%{cfg.buildcfg}")
+    objdir ("bin-obj/%{cfg.buildcfg}")
 
 	files
 	{
 		"src/**.h",
 		"src/**.cpp",
-		
 		"include/**.h"
 	}
 
@@ -24,21 +26,17 @@ project "yaml-cpp"
 		"YAML_CPP_STATIC_DEFINE"
 	}
 
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "off"
-
 	filter "system:linux"
 		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
+
+    filter "configurations:Debug"
+        symbols "On"
+        optimize "Off"  -- Turn off optimization for debug builds
+		staticruntime "on"
+        runtime "Debug"
+
+    filter "configurations:Release"
+        symbols "Off"
+        optimize "On"  -- Full optimization for release builds
 		staticruntime "off"
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
+        runtime "Release"
